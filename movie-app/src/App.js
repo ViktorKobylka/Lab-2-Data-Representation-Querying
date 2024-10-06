80ef753ce9942620b0567b33e3cab745a1738632
ViktorKobylka <g00425163@atu.ie>
2024-10-06 18:05:01 +0000

    Updated App.js (error with node_modules)

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,20 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavigationBar from './components/navigationBar';
 import Header from './components/header';
-import Content from './components/content';
 import Footer from './components/footer';
-
+import Content from './components/content';
 
 function App() {
-
-
   return (
-    
-    <div className="App">
-      <Header />
-      <Content />
+    <Router>
+      <NavigationBar />
+      <Routes>
+        <Route path="/home" element={<Content />} />
+        <Route path="/read" element={<h1>Read Component</h1>} />
+        <Route path="/create" element={<h1>Create Component</h1>} />
+      </Routes>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
