24ad6522ec2b60351b86e0393fb92cd608449ee5
ViktorKobylka <g00425163@atu.ie>
2024-10-06 18:09:32 +0000

    ex6 Client Side Routing

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -10,8 +10,8 @@ function App() {
       <NavigationBar />
       <Routes>
         <Route path="/home" element={<Content />} />
-        <Route path="/read" element={<h1>Read Component</h1>} />
-        <Route path="/create" element={<h1>Create Component</h1>} />
+        <Route path="/read" element={<Footer />} />
+        <Route path="/create" element={<Header />} />
       </Routes>
       <Footer />
     </Router>
